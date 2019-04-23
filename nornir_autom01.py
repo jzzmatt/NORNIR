4085d0f8462f977f1796bb2d14f2c6d44c2aaf1a
jzzmatt <mateusjunior.ns@gmail.com>
2019-04-23 18:06:43 +0100

    setup CORE SPINE

--- nornir_autom01.py
+++ nornir_autom01.py
@@ -2,6 +2,7 @@
 
 from nornir import InitNornir
 from nornir.plugins.functions.text import print_title, print_result
+from nornir.plugins.tasks.data import load_yaml
 from nornir.plugins.tasks import networking, text
 from nornir.core.filter import F
 import yaml
@@ -32,10 +33,17 @@ def trash():
     result = nr.filter(site="VICTORIA", role="test-device")
     print(result.inventory.hosts)
 
+    # print_result(result)
+    # print(nr.inventory.hosts['CSR1000v-lab'].keys())
+    # print(nr.inventory.hosts['CSR1000v-lab']['facts']['facts']['interface_list'])
+
 def get_host_fact(task):
     get_fact = task.run(task=networking.napalm_get, getters=['facts'])
-    #task.host['facts'] = get_fact.result
+    #Keep fact on host variable
+    task.host['facts'] = get_fact.result
+    #Keep fact on FACT FOLDER
     savetodir(convertoyaml(get_fact.result), dir_fact, task.host)
+
 def get_backup(task):
     show_command_lst = [
         'terminal length 0',
@@ -49,7 +57,6 @@ def get_backup(task):
 
     #result = task.run(task=networking.netmiko_send_command(sh_command))
     savetodir(output, dir_backup, task.host)
-
 def convertoyaml(file):
     converted_f = yaml.dump(file)
     return converted_f
@@ -73,23 +80,47 @@ def savetodir(file_name, dir_name, hostname):
     with open(f'{hostname}{offset}', 'w') as f:
         f.write(file_name)
     print(f'Have successfully save {file_name} to {dir_name}')
+def send_template(task):
+    #Load FACT file into Host Attributes
+    fact_file = task.run(
+        task=load_yaml,
+        file=f'{dir_fact}/{task.host}_fact.yml'
+    )
+    task.host['facts'] = fact_file.result
+    # SETUP INTERFACES
+    r = task.run(
+        task=text.template_file,
+        name="Interface Configuration",
+        template="base_intf.j2",
+        path="./templates/"
+    )
+    #Save the Compile Config to FACT DIR
+    savetodir(r.result, dir_config, task.host)
 
 def main():
     print_title("Deploy INFRA DIRECTORY")
-    #Deploy INFRA DIRECTORY
     deploy_directory()
-    #RUN DEVICE FACT
-    print_title("Get Host Fact Using NAPALM")
-    result = nr.filter(site="VICTORIA", role="test-device").run(task=get_host_fact)
-    print_result(result)
 
-    #RUN A PRE BACKUP BEFORE CHANGES
-    print_title("Perform a Backup before Changes")
-    nr.filter(site="VICTORIA", role="test-device").run(task=get_backup)
+    print_title("Get Host Fact Using NAPALM")
+    result = nr.filter(site="CUCA", role="core-switch").run(task=get_host_fact)
     #print_result(result)
-    #file = nr.inventory.hosts['CSR1000v-lab']['facts']
+
+    print_title("Perform a Backup before Changes")
+    nr.filter(site="CUCA", role="core-switch").run(task=get_backup)
+
+    print_title("Send Template to HOST")
+    output = nr.filter(site="CUCA", role="core-switch").run(task=send_template)
+    print_result(output)
+
 
 if __name__ == '__main__':
-    main()
+    print_title("Send Template to HOST")
+    output = nr.filter(site="CUCA", role="core-switch").run(task=send_template)
+    print_result(output)
+    #main()
+    #print(nr.inventory.hosts['CSR1000v-lab']['interfaces'])
+
+
+
 
 
